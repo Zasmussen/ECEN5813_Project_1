e257063a2b402590cf1d2812d51dbc453ea28e59
zasmussen <zaas3933@colorado.edu>
2018-02-11 15:35:41 -0700

    Deleted includes in main.c

--- src/main.c
+++ src/main.c
@@ -1,13 +1,6 @@
-#include "MKL25Z4.h"
+
 #include "project1.h"
-#include <stdint.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include "conversion.h"
-#include "data.h"
-#include "debug.h"
-#include "memory.h"
-#include "platform.h"
+
 
 int main(void)
 {
